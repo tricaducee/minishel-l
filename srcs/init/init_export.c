fd40b76945ae757fa8227d3375328207a73b66f5
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-10-29 15:18:38 +0200

    small updt

--- srcs/init/init_export.c
+++ srcs/init/init_export.c
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:29:53 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/10/28 08:31:39 by lgenevey         ###   ########.fr       */
+/*   Updated: 2022/10/29 15:18:00 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,13 @@ t_list	*init_export(t_list *env)
 		ft_lstadd_back(&export, ft_lstnew(env_cpy->content));
 		env_cpy = env_cpy->next;
 	}
+	ft_lstadd_back(&export, ft_lstnew("OLDPWD"));
 	sort_alphabetically(export);
 	return (export);
 }
 
 
 // free les strings des noeuds depuis export ca va aussi le faire pour env
-// car il y aura plus de noeud ici
+// car il est possible qu'il y ait plus de noeuds dans export
 
 // par contre free les noeuds des deux cotes

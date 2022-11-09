83f48373b2aecde955f2c161562810d71734e57a
Hermès Rolle <tricaducee@gmail.com>
2022-11-09 14:06:46 +0100

    add_wildcard in work

--- srcs/parsing/split_cmd.c
+++ srcs/parsing/split_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrolle <hrolle@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hermesrolle <hermesrolle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 06:43:55 by hrolle            #+#    #+#             */
-/*   Updated: 2022/11/06 15:28:27 by hrolle           ###   ########.fr       */
+/*   Updated: 2022/11/09 05:10:02 by hermesrolle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,13 @@ char	*split_cmd_sp_add_func(char **cmdline, char *ret, unsigned int *i)
 {
 	if ((*cmdline)[*i] && (*cmdline)[*i] == '$')
 		return (add_var(cmdline, ret, i));
-	//else if ((*cmdline)[*i] && (*cmdline)[*i] == '*')//------------------------------------
-	//	return (add_wildcard(cmdline, ret, i));
+	// else if ((*cmdline)[*i] && (*cmdline)[*i] == '*')//------------------------------------
+	// {
+	// 	(*i)++;
+	// 	if (!ret)
+	// 		ret = ft_strdup("");
+	// 	return (add_wildcard(cmdline, ret, i));
+	// }
 	else if ((*cmdline)[*i] && (*cmdline)[*i] == '\'')
 		return (add_quote(cmdline, ret, i));
 	else if ((*cmdline)[*i] && (*cmdline)[*i] == '"')
@@ -65,6 +70,9 @@ char	*split_cmd_sp(char **cmdline, unsigned int *i)
 	char			*ret;
 
 	ret = NULL;
+	// ret = check_and_get_wildcard(NULL);
+	// if (ret)
+	// 	return (ret);
 	while ((*cmdline)[*i] && (*cmdline)[*i] != ' ' && (*cmdline)[*i]
 		!= '<' && (*cmdline)[*i] != '>' && (*cmdline)[*i]
 		!= '|' && (*cmdline)[*i] != '&')
